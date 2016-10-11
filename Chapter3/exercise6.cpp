1e34b273a18f1e7d15b0142399c39a4c3c813148
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-10-11 11:46:59 -0400

    Update exercise6.cpp

--- Chapter3/exercise6.cpp
+++ Chapter3/exercise6.cpp
@@ -6,32 +6,37 @@
 
 /* Problem Statement
 The Josephus problem is the following game:
-N people, numbered 1 to N, are sitting in a circle. Starting at person 1, a hot potato is passed. After M passes, the person holding the hot potato is eliminated,
-the circle closes ranks, and the game continues with the person who was sitting after the eliminated person picking up the hot potato. The last remaining person wins.
-Thus, if M = 0 and N = 5, players are eliminated in order, and player 5 wins. If M = 1and N = 5, the order of elimination is 2, 4, 1, 5.
+N people, numbered 1 to N, are sitting in a circle. Starting at person 1, a hot potato is passed. After M passes, the person holding the hot potato is eliminate, the circle closes ranks, and the game continues with the person who was sitting after the eliminated person picking up the hot potato. The last remaining person wins. Thus, if M = 0 and N = 5, players are eliminated in order, and player 5 wins. If M = 1 and N = 5, the order of elimination is 2, 4, 1, 5.
 a.  Write a program to solve the Josephus problem for general values of M and N. Try to make your program as efficient as possible.
 Make sure you dispose of cells.
 b.  What is the running time of your program?
-c.  If M = 1, what is the running time of your program? How is the actual speed affected by the delete routine for large values of
-N ( N > 100,000)?
+c.  If M = 1, what is the running time of your program? How is the actual speed affected by the delete routine for large values of N ( N > 100,000)?
 */
 
 #include <iostream>
 
 using namespace std;
 
+/*====================BEGIN PERSON====================*/
 class Person {
+	friend class Josephus;
+	
 public:
   unsigned getPos(void);
 
-private:
-  Person(unsigned pos, Person *prevPerson = 0, Person *currentPerson = 0, Person *nextPerson = 0){
+  /*Person(int pos){
     position = pos;
-    
+    //
+  }*/
+  
+private:
+  Person(int pos, Person *nextPerson = 0, Person *prevPerson = 0){
+    position = pos;
   }
+
+
   
   unsigned position;
-  Person *current;
   Person *next;
   Person *previous;
 };
@@ -39,11 +44,84 @@ private:
 unsigned Person::getPos(void){
   return position;
 }
+/*=====================END PERSON=====================*/
+
+/*===================BEGIN Josephus===================*/
+class Josephus {
+public:
+	class iterator {
+		friend class Josephus;
+	
+	public:
+		Person *current;
+		
+		iterator() : current(NULL){}
+		
+		const Person & operator*() const {
+			return *this->current;
+		}
+		
+		iterator operator++() {
+			iterator old = *this;
+			++(*this);
+			return old;
+		}
+		
+		bool operator==(const iterator & rhs) const {
+			return current == rhs.current;
+		}
+		
+		bool operator!=(const iterator & rhs) {
+			return !(*this == rhs);
+		}
+		
+		Person retrieve() const {
+			return current->position;
+		}
+		
+		iterator(Person *p) : current(p){}
+	};
+	
+	Josephus() {
+		people = 0;
+	}
+	
+	~Josephus() {
+		remove();
+	}
+	
+	void remove(void);
+		
+	private:
+		iterator itr;
+		Person *people;
+		Person *atEndItem;
+		
+private:
+	
+};
+
+void Josephus::remove(void){
+	Person *pt = people;
+	
+	while(pt) {
+		Person *temp = pt;
+		pt = pt->next;
+		delete temp;
+	}
+	
+	people = atEndItem = 0;
+}
+/*====================END Josephus====================*/
+
+
 
 
 int main(void){
-  Person testPerson(2);
-  cout << testPerson.getPos() << endl;
+  Josephus testGame;
+  Josephus::iterator itr;
+  
+  //cout << testPerson->getPos() << endl;
 
   return 0;
 }
